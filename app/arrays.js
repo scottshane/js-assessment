e54dc4d99b932e0e88f28753b5774980fdd4fade
Scott Hoffman <scottshane.hoffman@gmail.com>
2014-01-13 21:12:52 -0800

    initial commit + completed arrays best Practices modules and objects

--- app/arrays.js
+++ app/arrays.js
@@ -4,58 +4,158 @@ define(function() {
   return {
     indexOf : function(arr, item) {
 
+        for (var i = 0; i < arr.length; i+=1) {
+            if(arr[i] === item) {
+                return i;
+            }
+        }
+        return -1
     },
 
     sum : function(arr) {
+        //ECMA3
+        // var total = 0;
+        // for (var i = 0; i < arr.length; i++) {
+        //    total += parseInt(arr[i], 10);
+        // };
+        // return total;
+        
+        //ECMA5
+        return arr.reduce(function (p, c, i, a) {
+            return (p + c)
+        });
 
     },
 
     remove : function(arr, item) {
+        var len = arr.length
+            , i;
 
+        for (i = 0; i < len; i += 1) {
+            if( arr[i] === item ) {
+                arr.splice(i, 1)
+            }
+        };
+        return arr;
     },
 
     removeWithoutCopy : function(arr, item) {
+        var arr     = arr.sort()
+            , len   = arr.length
+            , begin = null
+            , end   = null
+            , i;
+        
+        for (i = 0; i <len; i+=1) {
+            if(arr[i] === item) {
+               if(begin === null) {
+                  begin = i;  
+               }else {
+                end = i;
+               }
+            }
+        };
 
+        arr.splice(begin, end)
+        return arr;
     },
 
     append : function(arr, item) {
-
+       arr.push(item);
+       return arr;
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index, 0, item);
+        return arr;
     },
 
     count : function(arr, item) {
+        var arr     = arr.sort()
+            , len   = arr.length
+            , count = 0
+            , i;
 
+        for (i = 0; i < len; i+=1) {
+            if(arr[i] === item) {
+                count += 1;
+            }
+        };
+        return count;
     },
 
     duplicates : function(arr) {
+        var arr          = arr.sort()
+            , len        = arr.length
+            , duplicates = []
+            , i;
 
+        for (i = 0; i < len; i += 1) {
+            if(arr[i+1] === arr[i] && duplicates.indexOf(arr[i]) === -1) {
+                 duplicates.push(arr[i]);
+            }
+        };
+        return duplicates;
     },
 
     square : function(arr) {
+        // ECMA3
+        // var len = arr.length
+        //     , i = 0;
+        //     , result = [];
+
+        // for( i = 0; i < len; i += 1) {
+        //     result.push(arr[i] * arr[i])
+        // }
+        // return result;
+    
+        //ecma5
+        return result = arr.map(function (e, i, a) {
+            return e * e; 
+        })
 
     },
 
     findAllOccurrences : function(arr, target) {
+       /* ECMA3
+         var len = arr.length
+              , result = []
+              , i;
 
+          for (i = 0; i < len; i += 1) {
+              if(arr[i] === target) {
+                  result.push(i)
+              }
+          }
+        */
+        
+        //ECMA5
+        var result =  [];
+        arr.forEach(function (element, index) {
+            if(element === target) {
+                result.push(index);
+            }
+        });
+        return result;
     }
   };
 });
